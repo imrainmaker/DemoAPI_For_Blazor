339ef3806be086c49b883c07a369be569a0237d6
imrainmaker <bazinjeremy@gmail.com>
2023-06-15 14:56:04 +0200

    Docker

---
+++ Database1/dbo/Tables/Game.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Game] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Titre]      VARCHAR (100) NOT NULL,
+    [Genre]      VARCHAR (50)  NULL,
+    [DadeSortie] INT           NULL,
+    [Note]       INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [CK_Note] CHECK ([Note]>=(0) AND [Note]<=(5))
+);
+
---
+++ Database1/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Email]   VARCHAR (150)  NOT NULL,
+    [Pwd]     VARBINARY (64) NULL,
+    [Salt]    VARCHAR (100)  NULL,
+    [IsAdmin] BIT            DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
